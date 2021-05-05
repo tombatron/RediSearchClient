bab54d1617a8fcb8a09ded76307b464b67eb9f61
Tombatron <tommy.hanks@gmail.com>
2021-05-05 17:15:05 -0400

    Added consts for representing redisearch commands.

---
+++ RediSearchClient/RediSearchCommands.cs
@@ -0,0 +1,51 @@
+namespace RediSearchClient
+{
+    internal static class RediSearchCommands
+    {
+        internal const string CREATE = "FT.CREATE";
+
+        internal const string SEARCH = "FT.SEARCH";
+
+        internal const string AGGREGATE = "FT.AGGREGATE";
+
+        internal const string EXPLAIN = "FT.EXPLAIN";
+
+        internal const string ALTER = "FT.ALTER";
+
+        internal const string DROPINDEX = "FT.DROPINDEX";
+
+        internal const string ALIASADD = "FT.ALIASADD";
+
+        internal const string ALIASUPDATE = "FT.ALIASUPDATE";
+
+        internal const string ALIASDEL = "FT.ALIASDEL";
+
+        internal const string TAGVALS = "FT.TAGVALS";
+
+        internal const string SUGADD = "FT.SUGADD";
+
+        internal const string SUGGET = "FT.SUGGET";
+
+        internal const string SUGDEL = "FT.SUGDEL";
+
+        internal const string SUGLEN = "FT.SUGLEN";
+
+        internal const string SYNUPDATE = "FT.SYNUPDATE";
+
+        internal const string SYNDUMP = "FT.SYNDUMP";
+
+        internal const string SPELLCHECK = "FT.SPELLCHECK";
+
+        internal const string DICTADD = "FT.DICTADD";
+
+        internal const string DICTDEL = "FT.DICTDEL";
+
+        internal const string DICTDUMP = "FT.DICTDUMP";
+
+        internal const string INFO = "FT.INFO";
+
+        internal const string LIST = "FT._LIST";
+
+        internal const string  CONFIG = "FT.CONFIG";
+    }
+}
