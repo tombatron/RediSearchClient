5d88a7ce7082719330c50d591616520ebf46e9aa
Tombatron <tommy.hanks@gmail.com>
2021-05-09 09:34:46 -0400

    Added initial integration test for the sync version of the CreateIndex command.

---
+++ RediSearchClient.IntegrationTests/CreateIndex.cs
@@ -0,0 +1,43 @@
+using RediSearchClient.Indexes;
+using StackExchange.Redis;
+using System;
+using Xunit;
+
+namespace RediSearchClient.IntegrationTests
+{
+    public class CreateIndex : IDisposable
+    {
+        private ConnectionMultiplexer _muxr;
+        private IDatabase _db;
+
+        private void Setup()
+        {
+            _muxr = ConnectionMultiplexer.Connect("localhost");
+
+            _db = _muxr.GetDatabase(0);
+        }
+
+        private void TearDown()
+        {
+            _muxr.Dispose();
+        }
+
+        [Fact]
+        public void WillCreateASimpleIndex()
+        {
+            var indexName = Guid.NewGuid().ToString("n");
+
+            var index = RediSearchIndex
+                .On(RediSearchStructure.HASH)
+                .ForKeysWithPrefix("Bogus::")
+                .WithSchema(x => x.Text("field_one"))
+                .Build();
+
+            _db.CreateIndex(indexName, index);
+        }
+
+        public CreateIndex() => Setup();
+
+        public void Dispose() => TearDown();
+    }
+}
