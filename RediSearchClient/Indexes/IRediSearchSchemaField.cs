d73b977a1da39bc4f0817dcfb50dea2909b5f707
Tombatron <tommy.hanks@gmail.com>
2021-05-09 13:22:32 -0400

    More documentation.

--- RediSearchClient/Indexes/IRediSearchSchemaField.cs
+++ RediSearchClient/Indexes/IRediSearchSchemaField.cs
@@ -1,7 +1,15 @@
 namespace RediSearchClient.Indexes
 {
+    /// <summary>
+    /// Contract representing a field definition on a RediSearch schema.
+    /// </summary>
     public interface IRediSearchSchemaField
     {
+        /// <summary>
+        /// The collection of arguments that will be passed as the definition
+        /// of the schema field through the `FT.CREATE` command.
+        /// </summary>
+        /// <value>Collection of schema field arguments.</value>
         object[] FieldArguments { get; }
     }
 }
--- RediSearchClient/Indexes/Language.cs
+++ RediSearchClient/Indexes/Language.cs
@@ -1,11 +1,36 @@
 namespace RediSearchClient.Indexes
 {
-public enum Language
-{
-	None,
-	English,
-	French,
-	Portuguese,
-	Spanish
-}
+    /// <summary>
+    /// An enumeration indicating supported languages for a search index.
+    /// 
+    /// `None` will resort to "default" which in this case would essentially
+    /// be "English".
+    /// </summary>
+    public enum Language
+    {
+        /// <summary>
+		/// No language specified.
+        /// </summary>
+        None,
+
+        /// <summary>
+		/// English is specified as the default language for the search index.
+        /// </summary>
+        English,
+
+        /// <summary>
+		/// French is specified as the default language for the search index.
+        /// </summary>
+        French,
+
+        /// <summary>
+		/// Portuguese is specified as the default language for the search index.
+        /// </summary>
+        Portuguese,
+
+        /// <summary>
+		/// Spanish is specified as the default language for the search index.
+        /// </summary>
+        Spanish
+    }
 }
