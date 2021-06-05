62b89d7d1f351422fe4c2e5b4f09f66948776a42
Tombatron <tommy.hanks@gmail.com>
2021-06-05 07:07:50 -0400

    Added in some documentation here.

--- RediSearchClient/SearchResultItem.cs
+++ RediSearchClient/SearchResultItem.cs
@@ -3,10 +3,21 @@ using StackExchange.Redis;
 
 namespace RediSearchClient
 {
+    /// <summary>
+    /// Describes a single search result. 
+    /// </summary>
     public class SearchResultItem
     {
+        /// <summary>
+        /// The key to the HASH or JSON object that was indexed by RediSearch. 
+        /// </summary>
+        /// <value></value>
         public string DocumentKey { get; private set; }
 
+        /// <summary>
+        /// The fields returned from the search index. 
+        /// </summary>
+        /// <value></value>
         public IDictionary<string, RedisResult> Fields { get; private set; }
 
         internal static SearchResultItem Create(string documentKey, RedisResult[] rawFields)
