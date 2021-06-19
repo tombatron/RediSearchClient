20cd9d15627ac5b64b3f4f1aa0398e1fa4468e0e
Tombatron <tommy.hanks@gmail.com>
2021-06-19 09:58:05 -0400

    Added in some testing for the new aggregation result.

---
+++ RediSearchClient.Tests/AggregateResultCollectionTests.cs
@@ -0,0 +1,91 @@
+using Xunit;
+using RediSearchClient;
+using StackExchange.Redis;
+
+namespace RediSearchClient.Tests
+{
+    public class AggregateResultCollectionTests
+    {
+        [Fact]
+        public void ItCanHandleNullAggregationResult()
+        {
+            var result = new AggregateResultCollection(null);
+
+            Assert.Equal(0, result.Count);
+        }   
+
+        [Fact]
+        public void ItCanHandleEmptyAggregationResult()
+        {
+            var emptyRedisResult = CreateEmptyRedisResultArray();
+
+            var result = new AggregateResultCollection(emptyRedisResult);
+
+            Assert.Equal(0, result.Count);
+        }
+
+        [Fact]
+        public void ItCanAccessResultByIndex()
+        {
+            var realishResult = CreateRealLookingRedisResultArray();
+
+            var result = new AggregateResultCollection(realishResult);
+
+            Assert.Equal("moon", (string)result[1]);
+        }
+
+        [Fact]
+        public void ItCanAccessResultByKeyName()
+        {
+            var realishResult = CreateRealLookingRedisResultArray();
+
+            var result = new AggregateResultCollection(realishResult);
+
+            Assert.Equal("world", (string)result["hello"]);
+        }
+
+        [Fact]
+        public void NonExistantResultByIndexReturnsDefault()
+        {
+            var realishResult = CreateRealLookingRedisResultArray();
+
+            var result = new AggregateResultCollection(realishResult);
+
+            Assert.Equal(default, (string)result[100]);
+        }
+
+        [Fact]
+        public void NonExistantResultByKeyNameReturnsDefault()
+        {
+            var realishResult = CreateRealLookingRedisResultArray();
+
+            var result = new AggregateResultCollection(realishResult);
+
+            var whatever = result["supdawg"];
+
+            Assert.Equal(default, (string)result["supdawg"]);
+        }
+
+        private RedisResult[] CreateEmptyRedisResultArray()
+        {
+            var item = RedisResult.Create(new RedisValue[0]);
+
+            return (RedisResult[])item;
+        }
+
+        private RedisResult[] CreateRealLookingRedisResultArray()
+        {
+            var values = new RedisValue[4]
+            {
+                new RedisValue("hello"),
+                new RedisValue("world"),
+                new RedisValue("goodnight"),
+                new RedisValue("moon")
+            };
+
+            var item = RedisResult.Create(values);
+
+            return (RedisResult[])item;
+        }
+    }
+}
