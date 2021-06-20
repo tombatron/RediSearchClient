51fd56a6107346250e35fcf5769aa729ac142e2a
Tombatron <tommy.hanks@gmail.com>
2021-06-20 07:20:31 -0400

    Added documentation and formatted the code.

--- RediSearchClient/Aggregate/RediSearchAggregateQuery.cs
+++ RediSearchClient/Aggregate/RediSearchAggregateQuery.cs
@@ -1,8 +1,16 @@
 namespace RediSearchClient.Aggregate
 {
-public static class RediSearchAggregateQuery
-{
-	public static RediSearchAggregateQueryBuilder On(string indexName) =>
-		new RediSearchAggregateQueryBuilder(indexName);
-}
+	/// <summary>
+	/// This is a factory of sorts that kicks off the aggregate query builder.
+	/// </summary>
+    public static class RediSearchAggregateQuery
+    {
+		/// <summary>
+		/// Builder method for specifying the index to create an aggregation query for.
+		/// </summary>
+		/// <param name="indexName">The name of the index to apply the aggregation.</param>
+		/// <returns></returns>
+        public static RediSearchAggregateQueryBuilder On(string indexName) =>
+            new RediSearchAggregateQueryBuilder(indexName);
+    }
 }
