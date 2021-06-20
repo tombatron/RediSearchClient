549442b24c6f9e08a1aba4286c037c9f7b5a97af
Tombatron <tommy.hanks@gmail.com>
2021-06-20 07:18:21 -0400

    Added in a little documentation.
    Removed to override that wasn't being used.

--- RediSearchClient/Aggregate/RediSearchAggregateDefinition.cs
+++ RediSearchClient/Aggregate/RediSearchAggregateDefinition.cs
@@ -1,15 +1,13 @@
-using System.Linq;
-
 namespace RediSearchClient.Aggregate
 {
+    /// <summary>
+    /// Used as a container for the output of the aggregate query builder.
+    /// </summary>
     public sealed class RediSearchAggregateDefinition
     {
         internal object[] Fields { get; }
 
         internal RediSearchAggregateDefinition(object[] fields) =>
             Fields = fields;
-
-        public override string ToString() => 
-            string.Join(" ", Fields.Select(x => x.ToString()));
     }
 }
