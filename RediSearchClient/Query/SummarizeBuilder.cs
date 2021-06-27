ae27163b47854b0316e8e48fb5bba41b05c5ca11
Tombatron <tommy.hanks@gmail.com>
2021-06-27 15:49:15 -0400

    More documentation and upped the nuget package version.

--- RediSearchClient/Query/SummarizeBuilder.cs
+++ RediSearchClient/Query/SummarizeBuilder.cs
@@ -1,9 +1,16 @@
 namespace RediSearchClient.Query
 {
+    /// <summary>
+    /// Builder class for forumating the SUMMARIZE portion of a query.
+    /// </summary>
     public sealed class SummarizeBuilder
     {
         private string[] _fields;
 
+        /// <summary>
+        /// Builder method for specifying the fields to summarize.
+        /// </summary>
+        /// <param name="fields"></param>
         public void Fields(params string[] fields)
         {
             _fields = fields;
@@ -11,6 +18,10 @@ namespace RediSearchClient.Query
 
         private int _fragments;
 
+        /// <summary>
+        /// Builder method for specifying how many fragments should be returned. (Default is 3)
+        /// </summary>
+        /// <param name="fragments"></param>
         public void Frags(int fragments)
         {
             _fragments = fragments;
@@ -18,6 +29,10 @@ namespace RediSearchClient.Query
 
         private int _length;
 
+        /// <summary>
+        /// Builder method for specifying how many context words each fragment should contain. (Default is 20)
+        /// </summary>
+        /// <param name="length"></param>
         public void Length(int length)
         {
             _length = length;
@@ -25,11 +40,20 @@ namespace RediSearchClient.Query
 
         private string _separator;
 
+        /// <summary>
+        /// Builder method for specifying the string used to divide between individual summary snippets. The
+        /// default value is "...".
+        /// </summary>
+        /// <param name="separator"></param>
         public void Separator(string separator)
         {
             _separator = separator;
         }
 
+        /// <summary>
+        /// The constructed arguments from this builder.
+        /// </summary>
+        /// <returns></returns>
         public object[] FieldArguments => GenerateArguments();
 
         private object[] _fieldArguments;
