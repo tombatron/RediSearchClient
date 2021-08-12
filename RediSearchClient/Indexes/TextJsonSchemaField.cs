bb139643e5d4af1aadd8cb17c238983c37c0abd0
Tombatron <tommy.hanks@gmail.com>
2021-08-12 18:56:30 -0400

    Added in text field builder for json based indexes.

---
+++ RediSearchClient/Indexes/TextJsonSchemaField.cs
@@ -0,0 +1,102 @@
+namespace RediSearchClient.Indexes
+{
+    internal sealed class TextJsonSchemaField : IRediSearchSchemaField
+    {
+        private readonly string _jsonPath;
+        private readonly string _alias;
+        private readonly bool _sortable;
+        private readonly bool _nostem;
+        private readonly bool _noindex;
+        private readonly Language _phonetic;
+        private readonly double _weight;
+
+        public object[] FieldArguments => GenerateArguments();
+
+        public TextJsonSchemaField(string jsonPath, string alias, bool sortable, bool nostem, bool noindex, Language phonetic, double weight)
+        {
+            _jsonPath = jsonPath;
+            _alias = alias;
+            _sortable = sortable;
+            _nostem = nostem;
+            _noindex = noindex;
+            _phonetic = phonetic;
+            _weight = weight;
+        }
+
+        private object[] _fieldArguments;
+
+        private object[] GenerateArguments()
+        {
+            if (_fieldArguments == null)
+            {
+                var argumentLength = 4;
+                argumentLength += _sortable ? 1 : 0;
+                argumentLength += _nostem ? 1 : 0;
+                argumentLength += _noindex ? 1 : 0;
+                argumentLength += _phonetic == Language.None ? 0 : 2;
+                argumentLength += _weight == 1 ? 0 : 2;
+
+                _fieldArguments = new object[argumentLength];
+
+                var currentArgumentIndex = 0;
+
+                _fieldArguments[currentArgumentIndex] = _jsonPath;
+                _fieldArguments[++currentArgumentIndex] = "AS";
+                _fieldArguments[++currentArgumentIndex] = _alias;
+                _fieldArguments[++currentArgumentIndex] = "TEXT";
+
+                if (_nostem)
+                {
+                    _fieldArguments[++currentArgumentIndex] = "NOSTEM";
+                }
+
+                if (_weight != 1)
+                {
+                    _fieldArguments[++currentArgumentIndex] = "WEIGHT";
+                    _fieldArguments[++currentArgumentIndex] = _weight;
+                }
+
+                if (_phonetic != Language.None)
+                {
+                    _fieldArguments[++currentArgumentIndex] = "PHONETIC";
+                    _fieldArguments[++currentArgumentIndex] = GetMatcherForLanguage(_phonetic);
+                }
+
+                if (_sortable)
+                {
+                    _fieldArguments[++currentArgumentIndex] = "SORTABLE";
+                }
+
+                if (_noindex)
+                {
+                    _fieldArguments[++currentArgumentIndex] = "NOINDEX";
+                }
+
+            }
+
+            return _fieldArguments;
+        }
+
+        private const string DoubleMetaphoneForEnglish = "dm:en";
+        private const string DoubleMetaphoneForFrench = "dm:fr";
+        private const string DoubleMetaphoneForPortuguese = "dm:pt";
+        private const string DoubleMetaphoneForSpanish = "dm:es";
+
+        private static string GetMatcherForLanguage(Language language)
+        {
+            switch (language)
+            {
+                case Language.English:
+                    return DoubleMetaphoneForEnglish;
+                case Language.French:
+                    return DoubleMetaphoneForFrench;
+                case Language.Portuguese:
+                    return DoubleMetaphoneForPortuguese;
+                case Language.Spanish:
+                    return DoubleMetaphoneForSpanish;
+                default:
+                    return DoubleMetaphoneForEnglish;
+            }
+        }
+    }
+}
