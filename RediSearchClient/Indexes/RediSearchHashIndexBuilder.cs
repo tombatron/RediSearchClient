e64a6ac9e989ee821d08c851c7838d03c613ab95
Tombatron <tommy.hanks@gmail.com>
2021-08-29 09:50:42 -0400

    Hash and Json implementations of the index builder.

---
+++ RediSearchClient/Indexes/RediSearchHashIndexBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace RediSearchClient.Indexes
+{
+    /// <summary>
+    /// Essentially guides the index creation toward the path of HASH based.
+    /// </summary>
+    public class RediSearchHashIndexBuilder : BaseRediSearchIndexBuilder<RediSearchSchemaFieldBuilder>
+    {
+        /// <summary>
+        /// Returns the kind of structure that we're creating the index on.
+        /// </summary>
+        /// <returns></returns>
+        protected override string ResolveStructure() => "HASH";
+    }
+}
