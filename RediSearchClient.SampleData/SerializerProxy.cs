3976412d2a5f83c3100cf8199feb305af7cde199
Tombatron <tommy.hanks@gmail.com>
2021-08-29 09:49:25 -0400

    Added in serializer support for serializing null strings as empty strings.
    Changed the "RediSearchIndexBuilder" to be a base class to better
    support an index builder for JSON and HASH.

--- RediSearchClient.SampleData/SerializerProxy.cs
+++ RediSearchClient.SampleData/SerializerProxy.cs
@@ -1,4 +1,7 @@
-﻿using System.Text.Json;
+﻿#nullable enable
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using NReJSON;
 using StackExchange.Redis;
 
@@ -6,10 +9,32 @@ namespace RediSearchClient.SampleData
 {
     public sealed class SerializerProxy : ISerializerProxy
     {
-        public TResult Deserialize<TResult>(RedisResult serializedValue) =>
+        private static readonly JsonSerializerOptions _options;
+
+        static SerializerProxy()
+        {
+            _options = new JsonSerializerOptions();
+            
+            _options.Converters.Add(new NullToEmptyStringConverter());
+        }
+        
+        public TResult? Deserialize<TResult>(RedisResult serializedValue) =>
             JsonSerializer.Deserialize<TResult>((string) serializedValue);
 
         public string Serialize<TObjectType>(TObjectType obj) =>
-            JsonSerializer.Serialize(obj);
+            JsonSerializer.Serialize(obj, _options);
+    }
+
+    public sealed class NullToEmptyStringConverter : JsonConverter<string>
+    {
+        public override bool HandleNull => true;
+        
+        public override bool CanConvert(Type typeToConvert) => typeToConvert == typeof(string);
+
+        public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+            reader.TokenType == JsonTokenType.Null ? String.Empty : reader.GetString();
+
+        public override void Write(Utf8JsonWriter writer, string? value, JsonSerializerOptions options) =>
+            writer.WriteStringValue(value ?? string.Empty);
     }
 }
