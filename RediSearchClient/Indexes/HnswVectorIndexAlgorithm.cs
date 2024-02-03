37856700432a93578f1292081f9ac0d9fea6ebad
Tommy Hanks <tommy.hanks@gmail.com>
2024-02-03 18:14:00 -0500

    Starting on HNSW index definition.

---
+++ RediSearchClient/Indexes/HnswVectorIndexAlgorithm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RediSearchClient.Indexes
+{
+    internal class HnswVectorIndexAlgorithm : VectorIndexAlgorithm
+    {
+        private readonly VectorType _type;
+        private readonly int _dimensions;
+        private readonly DistanceMetric _distanceMetric;
+
+        private readonly int? _initialCap;
+        private readonly int? _m;
+        private readonly int? _efConstruction;
+        private readonly int? _efRuntime;
+        private readonly float? _epsilon;
+
+        internal HnswVectorIndexAlgorithm(
+            VectorType type,
+            int dimensions,
+            DistanceMetric distanceMetric,
+            int? initialCap,
+            int? m,
+            int? efConstruction,
+            int? efRuntime,
+            float? epsilon)
+        {
+            _type = type;
+            _dimensions = dimensions;
+            _distanceMetric = distanceMetric;
+            _initialCap = initialCap;
+            _m = m;
+            _efConstruction = efConstruction;
+            _efRuntime = efRuntime;
+            _epsilon = epsilon;
+        }
+
+        internal override object[] GenerateArguments()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RediSearchClient/Indexes/VectorIndexAlgorithm.cs
@@ -0,0 +1,30 @@
+﻿namespace RediSearchClient.Indexes
+{
+    public abstract class VectorIndexAlgorithm
+    {
+        internal abstract object[] GenerateArguments();
+
+        public static VectorIndexAlgorithm FLAT(
+            VectorType type,
+            int dimensions,
+            DistanceMetric distanceMetric,
+
+            int? initialCap = null,
+            int? blockSize = null)
+        {
+            return default;
+        }
+
+        public static VectorIndexAlgorithm HNSW(
+            VectorType type,
+            int dimensions,
+            DistanceMetric distanceMetric,
+
+            int? initialCap = null,
+            int? m = null,
+            int? efConstruction = null,
+            int? efRuntime = null,
+            float? epsilon = null) => 
+                new HnswVectorIndexAlgorithm(type, dimensions, distanceMetric, initialCap, m, efConstruction, efRuntime, epsilon);
+    }
+}
